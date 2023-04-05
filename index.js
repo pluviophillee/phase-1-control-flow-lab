e6223bc2cf1ab73b9ffd5dfdfde73c184f2b6e58
Ebla Ekerdiker <eblaekerdiker@gmail.com>
2023-04-05 14:44:34 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet < 400){
+    return 'This one is on me!';
+  }
+  else if ( 400 <= feet && feet < 2000){
+    return 'That will be twenty bucks.';
+  }
+  else if (2000 <= feet && feet < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if ( 2500 <= feet){
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let comparison = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return comparison;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
+  
 }
